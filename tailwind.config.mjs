a74d594a7c939cb995501d229a8346c9e6482ba5
phanthaibao <phanbao2648@gmail.com>
2026-01-17 22:28:33 +0700

    setup and config tech

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-anonymous-default-export */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      gridTemplateColumns:{
+        'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+      },
+    },
+  },
+  plugins: [],
+};
