e5b95ff8a46f59745515ffef7837e2dd26b89b12
phanthaibao <phanbao2648@gmail.com>
2026-03-04 22:29:29 +0700

    handle change avatar when sign in success, add 2 routes in menuitems

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,14 @@
 "use client"
 import React from "react";
-import { assets} from "@/assets/assets";
+import { assets, BagIcon, CartIcon} from "@/assets/assets";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
-import { useClerk } from "@clerk/nextjs";
+import { useClerk, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
 
-  const { isSeller, router } = useAppContext();
+  const { isSeller, router, user } = useAppContext();
   const { openSignIn } = useClerk()
 
   return (
@@ -39,10 +39,22 @@ const Navbar = () => {
 
       <ul className="hidden md:flex items-center gap-4 ">
         <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
-        <button onClick={openSignIn} className="flex items-center gap-2 hover:text-gray-900 transition">
-          <Image src={assets.user_icon} alt="user icon" />
-          Tài khoản
-        </button>
+        { 
+          user
+            ? <>
+              <UserButton>
+                <UserButton.MenuItems>
+                  <UserButton.Action label="Cart" labelIcon={<CartIcon />} onClick={() => router.push('/cart')}/>
+                </UserButton.MenuItems>
+                <UserButton.MenuItems>
+                  <UserButton.Action label="My Orders" labelIcon={<BagIcon />} onClick={() => router.push('/my-orders')}/>
+                </UserButton.MenuItems>
+              </UserButton>
+            </> 
+            : <button onClick={openSignIn} className="flex items-center gap-2 hover:text-gray-900 transition">
+              <Image src={assets.user_icon} alt="user icon" />
+              Tài khoản
+            </button>}
       </ul>
 
       <div className="flex items-center md:hidden gap-3">
