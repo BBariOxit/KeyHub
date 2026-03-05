e5e0a5813e086969272453645f11a25142816e9b
phanthaibao <phanbao2648@gmail.com>
2026-03-05 16:30:37 +0700

    handle inngest func to update and delete user data to a database

--- config/inngest.js
+++ config/inngest.js
@@ -26,3 +26,39 @@ export const syncUserCreation = inngest.createFunction(
     await User.create(userData)
   }
 )
+
+// inngest func to update user data to a database
+export const syncUserUpdation = inngest.createFunction(
+  {
+    id: 'update-user-from-clerk'
+  },
+  {
+    event: 'clerk/user.updated'
+  },
+  async ({event}) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + ' ' + last_name,
+      imageUrl: image_url
+    }
+    await connectDB()
+    await User.findByIdAndUpdate(id, userData)
+  }
+)
+
+// inngest func to delete user data to a database
+export const syncUserDeletion = inngest.createFunction(
+  {
+    id: 'delete-user-with-clerk'
+  },
+  { 
+    event: 'clerk/user.deleted'
+  },
+  async ({event}) => {
+    const { id } = event.data
+    await connectDB()
+    await User.findByIdAndDelete(id)
+  }
+)
